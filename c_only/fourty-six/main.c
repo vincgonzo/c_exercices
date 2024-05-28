717c11fc11cb2bd44bacbd45403893a6102ef035
vincgonzo <vincgonzo@hotmail.fr>
2024-05-28 11:08:54 +0200

    dev(c-46): open file

---
+++ c_only/fourty-six/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_STR_READ 256
+
+int main() {
+    FILE* pt_file = fopen("test.txt", "r");
+    int letter = 0;
+    signed char txt[MAX_STR_READ];
+
+    if(pt_file == NULL)
+        exit(1);
+
+    // first method
+    while((letter = fgetc(pt_file)) != EOF){
+        printf("%c", letter);
+    }
+
+    printf("\n-------- METHOD 2 ------------\n");
+    fseek(pt_file, 0, SEEK_SET);
+    // second method with fgets
+    while (fgets(txt, MAX_STR_READ, pt_file) != NULL)
+    {
+        printf("%s", txt);
+    }
+    
+
+    fclose(pt_file);
+
+    return 0;
+}
