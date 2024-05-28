2d72f7041790d45190e84accacaa39cbf8f7c25c
vincgonzo <vincgonzo@hotmail.fr>
2024-05-28 11:28:31 +0200

    dev(c-47): correction update

--- c_only/fourty-seven/correction.c
+++ c_only/fourty-seven/correction.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
-    //ouverture du fichier en lecture seul
-    FILE* pt_fichier= fopen("hello.txt", "r");
+    //ouverture du fichier en mode ajout
+    FILE* pt_fichier= fopen("hello.txt", "a");
 
     // Gestion des erreurs
     if(pt_fichier == NULL)
@@ -14,15 +13,9 @@ int main()
         return -1;
     }
 
-    printf("Contenu du fichier:\n");
-    int resultat = 0;
-
-    // Affichage du contenu du fichier
-    while(resultat != EOF)
-    {
-        resultat = fgetc(pt_fichier);
-        printf("%c", (char)resultat );
-    }
+    // Ajout du nouveau texte dans le fichier
+    fputs("Salut les codeurs,\n", pt_fichier );
+    fputs("bienvenue dans cette formation sur le C!\n", pt_fichier );
 
     //Fermeture du fichier
     fclose(pt_fichier);
