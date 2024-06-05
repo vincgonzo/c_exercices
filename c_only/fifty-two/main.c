66aa18cd7edd489370cf2baaa66b508fbc07e5aa
vincgonzo <vincgonzo@hotmail.fr>
2024-06-05 16:41:04 +0200

    dev(c-52): atomic variables against race condition

--- c_only/fifty-two/main.c
+++ c_only/fifty-two/main.c
@@ -1,44 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <unistd.h>
-
 #include <pthread.h>
 
-void* myturn( void* arg ){
-    //void pointer allocate memory
-    int *intptr = (int*) malloc(sizeof(int));
-    *intptr = 5;
-    for (size_t i = 0; i < 8; i++)
-    {
-        sleep(1);
-        printf("My turn ! %d %d \n", i, *intptr);
-        (*intptr)++;
-    }
+#define BIG 10000000UL
 
-    return intptr;
+#if __STDC_VERSION__ < 201112 || __STDC_NO_ATOMICS__ == 1
+#error "Help, atomics is not supported"
+#endif
+
+_Atomic __uint64_t counter = 0;
+
+void counttobig(){
+    for (size_t i = 0; i < BIG; i++)
+        counter += i;
 }
 
-
-void yourturn(){
-    for (size_t i = 0; i < 3; i++)
-    {
-        sleep(2);
-        printf("Your turn ! %d\n", i);
-    }
+void* thread_func( void* arg ){
+    counttobig();
+    return NULL;
 }
 
 int main(void) {
 
-    pthread_t nthread;
+    pthread_t thread1, thread2;
     int* result;
 
-    pthread_create(&nthread, NULL, myturn, NULL);
-    // myturn();
-    yourturn();
+    pthread_create(&thread1, NULL, thread_func, NULL);
+    pthread_create(&thread2, NULL, thread_func, NULL);
     
     //wait until the thread is done before exit
-    pthread_join(nthread, (void *) &result);
-    printf("Thread is end with %d\n", *result);
-    free(result);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    printf("End Result is %d\n", counter);
     return 0;
 }
