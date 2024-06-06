e5feb2d6c4ff16fc7dd0163cac68347dcde357a3
vincgonzo <vincgonzo@hotmail.fr>
2024-06-06 20:38:12 +0200

    dev(c-53): comments

--- c_only/fifty-three/main.c
+++ c_only/fifty-three/main.c
@@ -6,29 +6,24 @@
 
 #define BIG 10000000UL
 
-#if __STDC_VERSION__ < 201112 || __STDC_NO_ATOMICS__ == 1
-#error "Help, atomics is not supported"
-#endif
+__uint32_t counter = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-_Atomic __uint64_t counter = 0;
-
-void counttobig(){
-    for (size_t i = 0; i < BIG; i++)
-        counter += i;
-}
-
-void* thread_func( void* arg ){
-    counttobig();
+void * count_to_big(void *arg){
+    for (__uint32_t i = 0; i < BIG; i++){
+        pthread_mutex_lock(&lock); // lock to avoid concurrency problems
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
     return NULL;
 }
 
 int main(void) {
 
     pthread_t thread1, thread2;
-    int* result;
 
-    pthread_create(&thread1, NULL, thread_func, NULL);
-    pthread_create(&thread2, NULL, thread_func, NULL);
+    pthread_create(&thread1, NULL, count_to_big, NULL);
+    pthread_create(&thread2, NULL, count_to_big, NULL);
     
     //wait until the thread is done before exit
     pthread_join(thread1, NULL);
