caf742745174aba822ee348359d2a85470cb9555
vincgonzo <vincgonzo@hotmail.fr>
2024-12-16 11:39:55 +0100

    dev(cpp-s): race condition

--- cpp_only/security/best_pratices/main.cpp
+++ cpp_only/security/best_pratices/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <memory>
+#include <thread>
 
+int counter = 0;
+
+void increment(){
+    for(int i = 0; i < 100000;i++){
+        ++counter;
+    }
+}
 
 void noLeak(){
     std::unique_ptr<int[]> ptr(new int[10]); // automatically cleaned
@@ -8,6 +16,15 @@ void noLeak(){
 
 int main(){
     noLeak();
+
+
+    std::thread t1(increment);
+    std::thread t2(increment);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Final counter value <" << counter << "> " << std::endl; // explected 200000 but result is non-deterministic
     return 0;
 }
 
